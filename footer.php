f282be5e63d7f4d82280ceed45b36bef46cfca57
pawankumar.ncr@gmail.com <pawan.kumar@laitkor.com>
2024-02-26 18:58:51 +0530

    footer added

---
+++ footer.php
@@ -0,0 +1,3 @@
+<script type="text/javascript" src="Assets/js/jquery-3.6.1.min.js"></script>
+    <script type="text/javascript" src="Assets/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="Assets/js/custom.js"></script>
