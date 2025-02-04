f45a45c39b9ec83a69471f2c7b4d88b7fb4ec4fe
shuvrod564 <shuvrod564@gmail.com>
2025-02-05 03:31:40 +0600

    ui fixing

--- outlet_report_saleCenter.php
+++ outlet_report_saleCenter.php
@@ -38,9 +38,8 @@ include_once('script/outlet_report_saleCenter_script.php');
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="Assets/css/style.css">
     <link rel="stylesheet" href="Assets/css/style1.css">
-    <link rel="stylesheet" href="Assets/css/outlet-report-sale-center.css">
-
-
+    <link rel="stylesheet" href="Assets/css/outlet-report-sale-center.css?v=1">
+ 
 </head>
 
 <body class="mb-saleBg">
@@ -601,10 +600,10 @@ include_once('script/outlet_report_saleCenter_script.php');
                                 </div>
                                 <div class="col-md-5 var-Cstcol">
                                     <p class="sl-varDtl"><?php echo showOtherLangText('Variances'); ?></p>
-                                    <p class="sl-varDif"><?php echo getNumFormtPrice($varienceTotalAmt, $getDefCurDet['curCode'], 0, ''); ?>
+                                    <p class="sl-varDif this"><?php echo getNumFormtPrice($varienceTotalAmt, $getDefCurDet['curCode'], 0, ''); ?>
                                         <?php
                                         if ($varPosAmt > 0 || $varNegAmt < 0) {
-                                            echo '(<span style="color:red;">' . $varNegAmt . $getDefCurDet['curCode'] . '</span>, ' . $varPosAmt . $getDefCurDet['curCode'] . ')';
+                                            echo '(<span style="color:red;">' . $varNegAmt . $getDefCurDet['curCode'] . '</span>,' . '<span class="text-dark">' . $varPosAmt . $getDefCurDet['curCode'] . '</span>'.  ' )';
                                         }
                                         ?>
                                     </p>
